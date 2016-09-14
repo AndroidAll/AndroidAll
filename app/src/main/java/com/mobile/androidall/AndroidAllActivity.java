deefdbee86160ab7f4b711d2161fe8cfdd4b300d
shuaizhimin <shuaizhimin@weibangong.com>
2016-09-14 17:19:37 +0800

    首次提交

---
+++ app/src/main/java/com/mobile/androidall/AndroidAllActivity.java
@@ -0,0 +1,13 @@
+package com.mobile.androidall;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AndroidAllActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_all);
+    }
+}
